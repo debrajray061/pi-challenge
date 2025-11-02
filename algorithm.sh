0503c8a0f93037e7981d85f2f621eedc4f19145f
Debraj Ray <debrajray061@gmail.com>
2025-11-02 12:01:21 +0530

    Update algorithm.sh

--- algorithm.sh
+++ algorithm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI" | tee report.txt
-echo "3.14159" | tee -a report.txt
-
-# UNCOMMENT THE FOLLOWING LINE TO GET A MORE PRECISE CALCULATION OF PI. ALSO REMOVE THE PREVIOUS ECHO STATEMENTS.
-# echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+if command -v bc >/dev/null 2>&1; then
+    echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+else
+    echo "bc not found. Please install bc to compute π." | tee report.txt
+fi
